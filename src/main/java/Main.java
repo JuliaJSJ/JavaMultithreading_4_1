246b647a7d880ba8a0b4a57ea591fc7d6e36246b
JuliaJSJ <80544651+JuliaJSJ@users.noreply.github.com>
2022-12-13 09:17:10 -0500

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static final int CALLS = 60;
-    public static final int SPECIALISTS = 10;
+    public static final int OPERATORS = 20;
 
     public static void main(String[] args) {
         CallCenter center = new CallCenter();
@@ -8,7 +8,7 @@ public class Main {
             new Thread(null, center::call, "№ " + i).start();
         }
 
-        for (int i = 1; i <= SPECIALISTS; i++) {
+        for (int i = 1; i <= OPERATORS; i++) {
             new Thread(null, center::takeTheCall, "№ " + i).start();
         }
     }
